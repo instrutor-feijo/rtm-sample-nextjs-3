bcf7df3e7860cb91a73c4f1bb1d95c20c6b771bc
instrutor-feijo <ec2-user@ip-10-11-203-135.ap-southeast-1.compute.internal>
2023-05-11 18:40:57 +0000

    First Commit from Locofy Builder

---
+++ pages/_app.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  const muiTheme = createTheme();
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>rtm-sample</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+
+        <style>{`@import url("https://fonts.googleapis.com/css2?family=Jaldi:wght@400&display=swap");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+body {
+  margin: 0;
+  line-height: normal;
+}
+`}</style>
+      </Head>
+      <ThemeProvider theme={muiTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ pages/index.js
@@ -0,0 +1,112 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button as MuiButton, Icon, TextField } from "@mui/material";
+import { Dropdown, DropdownButton, SplitButton } from "react-bootstrap";
+
+const Cover = () => {
+  const onButtonContainedTextAndIcoClick = (e) => {
+    fetch("https://nodejs-api-beige.vercel.app/departamentos")
+      .then((response) => {
+        document.getElementById("result").innerHTML = response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        backgroundColor: "#fff",
+        width: "100%",
+        height: "768px",
+        overflow: "hidden",
+        textAlign: "left",
+        fontSize: "24px",
+        color: "#000",
+        fontFamily: "Jaldi",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "26px",
+          left: "0px",
+          backgroundColor: "#f3f3f3",
+          width: "1024px",
+          height: "768px",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          backgroundColor: "#020202",
+          width: "1024px",
+          height: "76px",
+        }}
+      />
+      <div style={{ position: "absolute", top: "96px", left: "32px" }}>
+        Edge Functions
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "18px",
+          left: "32px",
+          color: "#ff7a00",
+        }}
+      >
+        Real Time Manager
+      </div>
+      <DropdownButton
+        style={{
+          width: "121px",
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          top: "174px",
+          left: "32px",
+        }}
+        title="Dropdown"
+        variant="primary"
+        align="start"
+        drop="down"
+      >
+        <Dropdown.Item>A</Dropdown.Item>
+        <Dropdown.Item>B</Dropdown.Item>
+        <Dropdown.Item>C</Dropdown.Item>
+      </DropdownButton>
+      <MuiButton
+        style={{
+          position: "absolute",
+          top: "247px",
+          left: "32px",
+          cursor: "pointer",
+        }}
+        variant="contained"
+        name="btn"
+        id="btn"
+        color="primary"
+        endIcon={<Icon>arrow_forward_sharp</Icon>}
+        onClick={onButtonContainedTextAndIcoClick}
+      >
+        CALL API
+      </MuiButton>
+      <TextField
+        style={{ position: "absolute", top: "174px", left: "432px" }}
+        sx={{ width: 562 }}
+        color="primary"
+        variant="outlined"
+        multiline
+        name="result"
+        id="result"
+        label="Label"
+        placeholder="Textarea placeholder"
+        margin="none"
+      />
+    </div>
+  );
+};
+
+export default Cover;
